24eac654e98e9bcad4f7fe2509359f972182058d
Adityakbr01 <aditykbr01@gmail.com>
2024-10-06 20:24:17 +0530

    ""

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,11 @@ import cors from "cors";
 const app = express();
 
 // Middleware for parsing JSON request bodies
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
