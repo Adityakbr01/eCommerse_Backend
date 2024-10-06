836ac6e2211d5cc81f69677e2dff40bf9ae88112
Adityakbr01 <aditykbr01@gmail.com>
2024-10-06 14:17:26 +0530

    ""

--- src/index.ts
+++ src/index.ts
@@ -11,10 +11,10 @@ import cors from "cors";
 const app = express();
 
 // Middleware for parsing JSON request bodies
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookie());
-app.use(cors());
 // Set the view engine to EJS
 app.set("view engine", "ejs");
 
